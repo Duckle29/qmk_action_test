c6bf9f374b5cbcfecd63260eec621362387a2591
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2020-02-08 00:18:34 +0100

    stuff

--- lib/python/qmk/cli/pid.py
+++ lib/python/qmk/cli/pid.py
@@ -5,99 +5,13 @@ Generates a PID and replaces the one in the provided config.h files if VID is 0x
 
 import os
 import fileinput
-import tempfile
 import subprocess
 from pathlib import Path
 
-import re
-import hashlib
 import json
 
 from milc import cli
-
-VID_regex = re.compile(r"^#define\s+VENDOR_ID\s+(0x[0-9a-f]{4})(\s+/[/|*].*)*$", flags=re.IGNORECASE)
-PID_regex = re.compile(r"^(#define\s+PRODUCT_ID\s+0x)([0-9a-f]{4})(\s+/[/|*].*)*$", flags=re.IGNORECASE)
-
-
-def get_ids(config_file):
-    """Gets the VID and PID, if any, in the supplied config file.
-
-    Args:
-        config_file (str): The path to the config file to process.
-
-    Returns:
-        (tuple):
-
-            * VID (re.Match): VID found in the config. None if none in the config
-            * PID (re.Match): PID found in the config. None if none in the config
-    """
-    id_vid = None
-    id_pid = None
-    with open(config_file) as cf:
-        for line in cf:
-            if id_vid is None:
-                id_vid = re.match(VID_regex, line)
-            if id_pid is None:
-                id_pid = re.match(PID_regex, line)
-            if id_vid is not None and id_pid is not None:
-                break
-    return id_vid, id_pid
-
-
-def calculate_pid(config_path, data, max_tries=10):
-    """Calculate a PID given the path to a config.h
-
-    This function generates a unique PID for any config.h supplied.
-    The PID is determined based on the SHA1 hexdigest of the path to the config file
-    The first 4 characters (2 bytes) are used as the PID, unless those collide with
-    an already assigned PID. If a collision happens the process will be repeated with offset + 1
-
-    Args:
-        config_path (str): The path to the config file to process.
-        data (dict):
-
-            * PID (str): The parent folder of the config associated with the PID key
-
-        max_tries (int): The maximum amount of attempts to get around collisions
-
-    Returns:
-        new_pid (str): The 4 characters of the 2 byte pid
-    """
-    m = hashlib.sha1()
-    m.update(config_path.encode('utf-8'))
-    digest = m.hexdigest()
-    offset = 0
-    while offset < max_tries:
-        id_pid = digest[offset:offset + 4].upper()
-
-        if id_pid not in data['pids']:
-            return id_pid
-        else:
-            offset += 1
-
-    raise RecursionError(offset)
-
-
-def atomic_dump(data, json_file):
-    """Dumps a dict as a json file atomically
-
-    This function should avoid data loss caused by an error happening during
-    file writing.
-    It accomplishes this by writing the new json file to a temporary file,
-    then using os.replace() to overwrite the old file with the new file.
-    os.replace() is atomic as long as the two files are on the same filesystem.
-    In case of an error before os.replace() the temp file isn't removed.
-
-    Args:
-        data (dict):
-
-            * PID (str): The parrent folder of the config associated with the PID key
-
-        json_file (str): The file to dump to
-    """
-    with tempfile.NamedTemporaryFile('w', dir=str(Path(json_file).parents[0]), delete=False) as tf:
-        json.dump(data, tf)
-    os.replace(tf.name, json_file)
+from qmk.keyboard import get_ids, calculate_pid, atomic_dump
 
 
 def replace_pid(config_h, id_pid, pid_match):
@@ -108,45 +22,10 @@ def replace_pid(config_h, id_pid, pid_match):
         id_pid (str): The PID to write in to the file as 4 characters
         pid_match (re.match): The full regex match for the PID string
     """
-    comment = pid_match.group(3) if pid_match.group(3) is not None else ''
 
+    comment = pid_match.group(2) if pid_match.group(2) is not None else ''
     for line in fileinput.input(config_h, inplace=True):
         print(line.replace(pid_match.group(0), "{}{}{}".format(pid_match.group(1), id_pid, comment)), end='')
-
-
-def init(json_path):
-    """Generates a skeleton pids json file.
-
-    Generates a skeleton jsonfile for this script. If the parent folders in the configured path don't exist, these will also be created.
-
-    Args:
-        json_path (str): The path to create the json skeleton file at
-    """
-
-    Path(json_path).parents[0].mkdir(parents=True, exist_ok=True)
-    with open(json_path, 'w') as jfile:
-        json.dump({"pids": {}}, jfile)
-    if cli.args.commit:
-        subprocess.run(
-            ['git', 'add', json_path],
-            timeout=5,
-            check=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT
-        )
-
-
-def lchop(source_string, chop):
-    """Chops off chop from thestring from the left
-
-        Args:
-            source_string (str): The string to check
-            chop (str): The string to remove from source_string
-    """
-
-    if source_string.startswith(chop):
-        return source_string[len(chop):]
-    return source_string
 
 
 def process_config(cli, config_path, pids_json_path, pid_match):
@@ -159,10 +38,8 @@ def process_config(cli, config_path, pids_json_path, pid_match):
             pid_match (re.Match): The full regex match of the product id define
     """
 
-    config_path_lchop = cli.config.pid.path_lchop if cli.config.pid.path_lchop else "keyboards/"
-
-    chopped_config_path = lchop(config_path, config_path_lchop)
-    keyboard = str(Path(config_path).parent.stem)
+    chopped_config_path = '/'.join(config_path.parts[1:])
+    keyboard = '/'.join(config_path.parts[1:-1])
 
     with open(pids_json_path, 'r') as json_file:
         data = json.load(json_file)
@@ -170,20 +47,20 @@ def process_config(cli, config_path, pids_json_path, pid_match):
     try:
         pid_new = calculate_pid(chopped_config_path, data)
     except RecursionError as e:
-        cli.log.error("Too many PID collisions ({}) when generating PID for {}. Aborting".format(e, keyboard))
+        cli.log.error("Too many PID collisions (%s) when generating PID for %s. Aborting", e, keyboard)
         return False
 
     if chopped_config_path in data['pids'].values():
         if pid_new != pid_match.group(2):
-            cli.log.warning("{} already assigned {}, but PID doesn't match.".format(keyboard, pid_new))
+            cli.log.warning("%s already assigned %s, but PID doesn't match.", keyboard, pid_new)
             if cli.args.apply:
                 replace_pid(config_path, pid_new, pid_match)
-                cli.log.warning('\tRe-applying {} to {}'.format(pid_new, keyboard))
+                cli.log.warning('\tRe-applying %s to %s', pid_new, keyboard)
         else:
-            cli.log.info('{} already assigned pid 0x{}'.format(keyboard, pid_new))
+            cli.log.info('%s already assigned pid 0x%s', keyboard, pid_new)
             return True
     else:
-        cli.log.info('Assigned PID 0x{} to {}'.format(pid_new, keyboard))
+        cli.log.info('Assigned PID 0x%s to %s', pid_new, keyboard)
         if cli.args.apply:
             cli.log.info('\tApplying PID')
             replace_pid(config_path, pid_new, pid_match)
@@ -194,14 +71,14 @@ def process_config(cli, config_path, pids_json_path, pid_match):
         try:
             cli.log.info('\tCommitting changes to git')
             subprocess.run(
-                ['git', 'commit', '-am', '"Generated PID 0x{} for {}"'.format(pid_new, keyboard)],
+                ['git', 'commit', '-am', 'Generated PID 0x%s for %s' % (pid_new, keyboard)],
                 timeout=5,
                 check=True,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.STDOUT
             )
         except subprocess.CalledProcessError as e:
-            cli.log.error('Committing to git failed. {} failed with: {}'.format(e.cmd, e.output))
+            cli.log.error('Committing to git failed. %s failed with: %s', e.cmd, e.output)
             return False
     return True
 
@@ -224,17 +101,14 @@ def pid(cli):
 
     # Make a skeleton json file on first run
     if not Path(pids_json_path).is_file():
-        try:
-            init(pids_json_path)
-        except subprocess.CalledProcessError as e:
-            cli.log.error('Adding json to git failed. {} failed with: {}'.format(e.cmd, e.output))
-            return False
+        cli.log.error('Assigned pids file not found %s', pids_json_path)
+        return False
 
     for config_h in cli.args.config:
         vid_match, pid_match = get_ids(config_h)
 
         if vid_match is None:
-            cli.log.info('{} is not a keyboard config'.format(config_h))
+            cli.log.info('%s is not a keyboard config', config_h)
             continue
 
         # If vid_match is not none, but pid_match is:
@@ -243,9 +117,10 @@ def pid(cli):
             return False
 
         if vid_match.group(1) != qmk_vid:
-            cli.log.info("Keyboard does not use QMK VID, or it's not a keyboard config. ({} != {} in {})".format(vid_match.group(1), qmk_vid, config_h))
+            cli.log.info("Keyboard does not use QMK VID, or it's not a keyboard config. (%s != %s in %s)", vid_match.group(1), qmk_vid, config_h)
             continue
         else:
-            if not process_config(cli, config_h, pids_json_path, pid_match):
+            if not process_config(cli, Path(config_h), pids_json_path, pid_match):
                 return False
     return True
+
