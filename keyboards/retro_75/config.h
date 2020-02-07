a24020c067f78d4182eb11c96163793248b0e6b8
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2020-02-08 00:27:36 +0100

    a

--- keyboards/retro_75/config.h
+++ keyboards/retro_75/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x03A8
-#define PRODUCT_ID      0x5275
+#define PRODUCT_ID      0x52A5
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    PheonixStarr
 #define PRODUCT         Retro75
